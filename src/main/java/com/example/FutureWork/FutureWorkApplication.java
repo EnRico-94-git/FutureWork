3bd683cb9c3903fc7d508b7ea39ef86e27840fe7
EnRico-94-git <egaldino2004@gmail.com>
2025-11-12 18:59:00 -0300

    initial commit

---
+++ src/main/java/com/example/FutureWork/FutureWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.FutureWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureWorkApplication.class, args);
+	}
+
+}
